c3f47b2a18b0a5134d74867bbf4e45dd68fb121a
doo <lv.yi@ikasinfo.com>
2022-01-14 15:04:46 +0800

    update hit rate up

--- src/main/java/com/doo/xenchant/mixin/PersistentProjectileEntityMixin.java
+++ src/main/java/com/doo/xenchant/mixin/PersistentProjectileEntityMixin.java
@@ -2,28 +2,23 @@ package com.doo.xenchant.mixin;
 
 import com.doo.xenchant.util.EnchantUtil;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PersistentProjectileEntity.class)
-public abstract class PersistentProjectileEntityMixin extends Entity {
-
-    public PersistentProjectileEntityMixin(EntityType<?> type, World world) {
-        super(type, world);
-    }
+public abstract class PersistentProjectileEntityMixin {
 
     @Inject(method = "setOwner", at = @At("TAIL"))
     private void setOwnerT(Entity entity, CallbackInfo ci) {
         if (entity instanceof ServerPlayerEntity) {
-            Box newBox = this.getBoundingBox().expand(EnchantUtil.hitRateUp(((ServerPlayerEntity) entity).getActiveItem()));
-            this.setBoundingBox(newBox);
+            Box newBox = ((PersistentProjectileEntity) (Object) this).getBoundingBox().expand(
+                    EnchantUtil.hitRateUp(((ServerPlayerEntity) entity).getActiveItem()));
+            ((PersistentProjectileEntity) (Object) this).setBoundingBox(newBox);
         }
     }
 }
